154452d369fa1f75f8a5a856ff9317c8eaf383da
Kasyanov-git <zabydy40@gmail.com>
2024-01-25 15:06:46 +0300

    new file:   .idea/vcs.xml 	renamed:    app.md -> app/app.md 	renamed:    auth.py -> app/core/auth.py 	renamed:    config.py -> app/core/config.py 	renamed:    database.py -> app/core/database.py 	renamed:    worker.py -> app/core/worker.py 	renamed:    main.py -> app/main.py 	renamed:    models.py -> app/models/models.py 	renamed:    schemas.py -> app/schema/schemas.py 	renamed:    prediction.py -> app/utils/prediction.py 	renamed:    preprocessing.py -> app/utils/preprocessing.py 	renamed:    webui/app.py -> app/webui/app.py 	renamed:    models/gb_model.joblib -> ml_models/gb_model.joblib 	renamed:    models/knn_model.joblib -> ml_models/knn_model.joblib 	renamed:    models/lr_model.joblib -> ml_models/lr_model.joblib
    	deleted:    __pycache__/auth.cpython-39.pyc
    	deleted:    __pycache__/config.cpython-39.pyc
    	deleted:    __pycache__/database.cpython-39.pyc
    	deleted:    __pycache__/main.cpython-39.pyc
    	deleted:    __pycache__/models.cpython-39.pyc
    	deleted:    __pycache__/prediction.cpython-39.pyc
    	deleted:    __pycache__/preprocessing.cpython-39.pyc
    	deleted:    __pycache__/schemas.cpython-39.pyc
    	deleted:    __pycache__/worker.cpython-39.pyc
    	modified:   app/core/auth.py
    	modified:   app/main.py
    	modified:   app/models/models.py
    	modified:   app/utils/prediction.py

--- app/core/database.py
+++ app/core/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-from config import DATABASE_URL
+from app.core.config import DATABASE_URL
 
 engine = create_engine(DATABASE_URL)
 
@@ -12,6 +12,7 @@ Base.metadata.create_all(bind=engine)
 
 # Создание сессии
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 
 def get_db():
     db = SessionLocal()
